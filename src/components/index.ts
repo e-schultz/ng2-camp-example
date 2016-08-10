973001cdf533ea29bbd1c015a4c9ca676f31510b
Evan Schultz <evan@rangle.io>
2016-08-10 19:45:40 -0400

    create orders module

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { LineupModule } from './lineup';
 import { PanelModule } from './panel';
 import { TableModule } from './table';
-import { Orders } from './orders';
-import { BrowserModule }  from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { OrdersModule } from './orders';
 import { MenuModule } from './menu';
+
 export * from './lineup';
 export * from './panel';
 export * from './table';
@@ -13,9 +12,9 @@ export * from './menu';
 export * from './orders';
 
 @NgModule({
-  imports: [BrowserModule, LineupModule, FormsModule, MenuModule, TableModule, PanelModule],
-  declarations: [Orders],
-  exports: [PanelModule, Orders, LineupModule, MenuModule, TableModule]
+  imports: [LineupModule, MenuModule, TableModule, PanelModule, OrdersModule],
+  declarations: [],
+  exports: [PanelModule, OrdersModule, LineupModule, MenuModule, TableModule]
 })
 export class ComponentsModule {
 
